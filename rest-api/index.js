d9bb10ad44f1b919fe57d55e4e64e9e0f415f911
Lief James Cain <you@example.com>
2021-09-23 12:37:42 -0700

    server + database + routes

---
+++ rest-api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+// NOTE: this is a workaround
+global.TextEncoder = require("util").TextEncoder;
+global.TextDecoder = require("util").TextDecoder;
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+//database
+mongoose.connect(process.env.MONGO_CONNECT, () => {
+  console.log("Mongo DB is connected!");
+});
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users/", userRoute);
+
+// server
+app.listen(8800, () => {
+  console.log("Express server is running!");
+});
